cb6d8e7df5e6083650ad77d4d6cfca4290a48aab
leonardosantosp <leo040604@gmail.com>
2025-05-12 20:37:30 -0300

    fix(ui): style Wikipedia data and resolve link conflict

--- front-end/elastic/src/App.tsx
+++ front-end/elastic/src/App.tsx
@@ -1,27 +1,26 @@
 import { MathTopics } from './components/MathTopics'
 import { SearchPanel } from './components/SearchPanel'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { ResultPages } from './pages/ResultsPage'
 import { WikiViewer } from './pages/WikiViewer'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <SearchPanel />
-                <MathTopics />
-              </>
-            }
-          />
-          <Route path="/result" element={<ResultPages />} />
-          <Route path="/wiki" element={<WikiViewer />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <SearchPanel />
+              <MathTopics />
+            </>
+          }
+        />
+
+        <Route path="/result" element={<ResultPages />} />
+        <Route path="/wiki" element={<WikiViewer />} />
+      </Routes>
     </>
   )
 }
--- front-end/elastic/src/main.tsx
+++ front-end/elastic/src/main.tsx
@@ -5,13 +5,16 @@ import App from './App.tsx'
 import { Header } from './components/Header.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
 import { Footer } from './components/Footer.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <Header />
-      <App />
-      <Footer />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Header />
+        <App />
+        <Footer />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 )
