33d0e370672d20b91fed0ebf4e5e13c61598af26
leonardosantosp <leo040604@gmail.com>
2025-05-13 13:55:05 -0300

    feat: add signup page

--- front-end/elastic/src/App.tsx
+++ front-end/elastic/src/App.tsx
@@ -4,12 +4,16 @@ import { Routes, Route } from 'react-router-dom'
 import { ResultPages } from './pages/ResultsPage'
 import { WikiViewer } from './pages/WikiViewer'
 import { LoginPage } from './pages/SignUpPage'
+<<<<<<< HEAD
 
+=======
+>>>>>>> c49c57f (feat: add signup page)
 
 function App() {
 
   return (
     <>
+<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route
@@ -26,6 +30,24 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
+=======
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <SearchPanel />
+              <MathTopics />
+            </>
+          }
+        />
+
+        <Route path="/result" element={<ResultPages />} />
+        <Route path="/wiki" element={<WikiViewer />} />
+
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+>>>>>>> c49c57f (feat: add signup page)
     </>
   )
 }
--- front-end/elastic/src/pages/SignUpPage.tsx
+++ front-end/elastic/src/pages/SignUpPage.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+<<<<<<< HEAD
 import { Login } from '../components/Login'
 import { useState } from 'react'
 
@@ -10,6 +11,22 @@ export const LoginPage = () => {
   return (
     <>
       <div className="login__header">
+=======
+import { EyeClosed, Eye, ArrowRight } from 'lucide-react'
+import google from '../assets/google.png'
+import planet from '../assets/planet-purple.png'
+import { useState } from 'react'
+
+export const LoginPage = () => {
+  const [viewPassword, setViewPassword] = useState(false)
+  const handdleViewPassword = () => {
+    setViewPassword(!viewPassword)
+  }
+
+  return (
+    <>
+      <div className="signup__header">
+>>>>>>> c49c57f (feat: add signup page)
         <Link to="/">
           <div className="header__logo">
             <span>π</span>
@@ -17,6 +34,7 @@ export const LoginPage = () => {
           </div>
         </Link>
       </div>
+<<<<<<< HEAD
       {loginType === 'login' ? (
         <Login
           title="Welcome Back !"
@@ -45,6 +63,79 @@ export const LoginPage = () => {
           setType={(type: 'login' | 'signup' | 'forgot') => setLoginType(type)}
         />
       )}
+=======
+      <div className="signup-page">
+        <div className="signup-page__container">
+          <div className="signup-page__title">
+            <h1>Welcome Back !</h1>
+            <div className="dashed-container">
+              <div className="dashed-line__text">
+                <p>Skip the lag?</p>
+              </div>
+              <div class="dashed-line"></div>
+            </div>
+          </div>
+          <img
+            src={planet}
+            alt="planet"
+            width={302}
+            height={302}
+            className="planet first-planet"
+          />
+          <div className="signup-page__card">
+            <h2>Login</h2>
+            <p>Glad you’re back!</p>
+            <input type="text" placeholder="Username" />
+            <div className="card__password">
+              <input type="password" placeholder="Password" />
+              <div className="card__password--icon-container">
+                {!viewPassword ? (
+                  <EyeClosed
+                    size={20}
+                    color="#ffff"
+                    onClick={() => handdleViewPassword()}
+                  />
+                ) : (
+                  <Eye
+                    size={20}
+                    color="#ffff"
+                    onClick={() => handdleViewPassword()}
+                  />
+                )}
+              </div>
+            </div>
+            <button className="login-button">Login</button>
+            <button className="forgot">Forgot password ?</button>
+            <div className="or">
+              <div className="or__header">
+                <div className="or__bar">{}</div>
+                <p>or</p>
+                <div className="or__bar">{}</div>
+              </div>
+              <div className="login-options">
+                <img src={google} alt="google " width={30} height={30} />
+              </div>
+            </div>
+            <div className="dont-have-account">
+              <p>Don’t have an account ? Signup</p>
+            </div>
+            <div className="login-guest-container">
+              <div className="login-guest">
+                <p>login as guest</p>
+                <ArrowRight color="white" className="arrow" />
+              </div>
+            </div>
+          </div>
+          <img
+            src={planet}
+            alt="planet"
+            width={220}
+            height={220}
+            className="planet second-planet"
+          />
+        </div>
+      </div>
+>>>>>>> c49c57f (feat: add signup page)
     </>
   )
 }
