f3e7c70d8c8d531c922f42d2cfc0c356c6893342
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-25 13:12:54 -0300

    feat: creating token validation

--- back-end/routes/authServer.ts
+++ back-end/routes/authServer.ts
@@ -1,41 +1,47 @@
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 
-export function authServer(app) {
+
+// faz as validacoes 
+// verifica se access token eh valido e faz serializa (da a permissao)
+// o token deve ser mandado no headder na requisicao
+
+export function authServer( app ) {
     dotenv.config()
-  
-    app.post('/login', async (req, res) => {
-      const { username, password } = req.body
-      const user = { username: username, password: password }
-  
-      //dados do usuário
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1min' })
-      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
-  
-      return res.status(200).send({ accessToken: accessToken, refreshToken: refreshToken, accessSecret: process.env.ACCESS_TOKEN_SECRET, refreshSecret:process.env.REFRESH_TOKEN_SECRET })
+
+    const users = [
+        {
+          db_username: 'Leo',
+          db_content: 'Leo123'
+        },
+        {
+          db_username: 'Ywao',
+          db_content: 'Ywao123'
+        }
+      ]
+
+    // verifica se o usuario esta em users e se o token no headder
+    // da requisicao bate com o ACCESS_TOKEN_SECRET
+    app.get('/authenticate', { preHandler: authenticateToken }, ( req, res ) => {
+      // TO-DO: procurar no banco async-await
+      const user = users.filter(u => u.db_username === req.user.username )
+      if ( user.length == 0 ) return res.code(404).send({message: "Usuario nao autenticado"})
+
+      return res.send(user)
     })
-  
-    // renovando ACCESS_TOKEN com REFRESH_TOKEN
-    app.post('/refresh', async (req, res) => {
-        const refreshToken = req.body.refreshToken
-        if( refreshToken == null ) return res.status(401)
-        // if( refreshTokens.includes( refreshToken ) ) return res.status(403)
-        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, ( err, user ) => {
-          console.log( err )
-          if ( err ) return res.json({ errorInVerification: err })
-          const newAccessToken = generateAccessToken({ name: user.username })
-          return res.send({ newAccessToken: newAccessToken })
+
+    function authenticateToken ( req, res, next ) {
+        const authHeader = req.headers['authorization'] // obtendo token no headder
+        const token = authHeader && authHeader.split(' ')[1]
+        if ( token == null ) return res.status( 401 )
+        
+        // verificando se token eh autenticado ( bate com o access secret )
+        jwt.verify( token, process.env.ACCESS_TOKEN_SECRET, ( err, user ) => {
+            if ( err ) return res.send({ message: err })
+            req.user = user
+            next() // sai do preHandler
         })
-    })
-  
-    // to-do deletar refresh tokens
-    app.delete('/logout', (req, res) => {
-      // refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-      res.sendStatus(204)
-    })
-  
-    function generateAccessToken( user ) {
-      return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' } )
     }
 }
-  
+
+
