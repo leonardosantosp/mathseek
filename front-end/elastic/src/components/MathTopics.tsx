85e07a8fa7aad2e074b99dce2b7cd821c9282c94
leonardosantosp <leo040604@gmail.com>
2025-05-25 19:55:19 -0300

    feat(math-topics): connect trending topics section to API and display most viewed documents

--- front-end/elastic/src/components/MathTopics.tsx
+++ front-end/elastic/src/components/MathTopics.tsx
@@ -1,67 +1,54 @@
 import { Search } from 'lucide-react'
 import { Pagination } from './Pagination'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+type Result = {
+  _id: number
+  title: string
+  url: string
+  content: string
+  reading_time: number
+  access_count: number
+  dt_creation: string
+}
 
 export const MathTopics = () => {
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(1)
   const size = 3
-  const numPages = 3
-  const firstIndex = page * size
-  const lastIndex = (page + 1) * size
-  const content = [
-    {
-      title: 'Treap1',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap2',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap3',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap4',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap5',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap6',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap7',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap8',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
-    },
-    {
-      title: 'Treap9',
-      text: 'In computer science, the treap and the randomized binary search tree are two closely related forms of binary search tree data structures that maintain a dynamic set of ordered keys and allow binary searches among the keys. After any sequence of insertions and deletions of keys, the shape of the tree is a random variable with the same probability distribution as a random binary tree;'
+  const [documents, setDocuments] = useState<Result[]>([])
+  const totalDocuments = 9
+  const numPages = Math.ceil(totalDocuments / size)
+
+  useEffect(() => {
+    const fetchMostViewDocuments = async () => {
+      const response = await fetch(
+        `http://localhost:3333/wikipedia/mostViews/${totalDocuments}`
+      )
+      setDocuments(await response.json())
     }
-  ]
+    fetchMostViewDocuments()
+  }, [])
+
+  const firstIndex = (page - 1) * size
+  const lastIndex = page * size
 
   return (
     <>
-      <h2 className="math-topics__title">Trending Math Topics</h2>
       <div className="math-topics__card">
+        <h2 className="math-topics__title">Trending Math Topics</h2>
         <div className="math-topics__card-container">
-          {content.slice(firstIndex, lastIndex).map(item => (
-            <div className="math-topics__card-item">
-              <div className="math-topics__card-item--header">
-                <p>{item.title}</p>
-                <Search className="math-topics__card-item--search" />
+          {documents.slice(firstIndex, lastIndex).map(document => (
+            <>
+              <div className="math-topics__card-item">
+                <div className="math-topics__card-item--header">
+                  <p>{document.title}</p>
+                  <Search className="math-topics__card-item--search" />
+                </div>
+                <div className="math-topics__card-item--text">
+                  <p>{document.content}</p>
+                </div>
               </div>
-              <div className="math-topics__card-item--text">
-                <p>{item.text}</p>
-              </div>
-            </div>
+            </>
           ))}
         </div>
 
@@ -70,6 +57,7 @@ export const MathTopics = () => {
           changePage={(newPage: number) => setPage(newPage)}
           numPages={numPages}
           pageInfo={false}
+          showNumbers={false}
         />
       </div>
     </>
