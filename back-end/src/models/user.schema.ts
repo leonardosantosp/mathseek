bcdfe60b95045b3b35f397edcfd3068d482f422b
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-27 14:22:22 -0300

    feat: creating user schema

---
+++ back-end/src/models/user.schema.ts
@@ -0,0 +1,48 @@
+import mongoose, { Document, Schema, Model } from "mongoose" 
+import { InterfaceConfig, configSchema } from "./config.schema";
+
+export interface InterfaceUser extends Document { // extends Document traz .save(), _id, .toJSON()
+  username: string,
+  password: string,
+  hashedPassword: string,
+  avatar?: BufferSource, // optional usuario pode ter foto padrao (sem foto) // arruma BufferSource
+  status?: string,
+  config: InterfaceConfig
+}
+
+// todo usuario vai ter um subschema de preferencia
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String, // Ref outside bd
+    required: false
+  },
+  status: {
+    type: String,
+    required: false
+  },
+  config: {
+    type: configSchema,
+    default: () => ({})
+  }
+})
+
+// userSchema.virtual('coverImagePath').get(function() {
+//     if (this.coverImage != null && this.coverImageType != null) {
+//         return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+//     }
+// })
+
+export const UserSchema: Model<InterfaceUser> =
+  mongoose.model<InterfaceUser>('User', userSchema)
