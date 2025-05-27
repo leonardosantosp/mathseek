8a935f4d5cf2725c52e877eafe66143b02b1bbed
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-27 15:17:04 -0300

    feat: implementing users config schema

--- back-end/src/models/config.schema.ts
+++ back-end/src/models/config.schema.ts
@@ -2,15 +2,38 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
 
-//tipagem do schema
+
+// tipagem do schema folders
+interface InterfaceFolders {
+    folderName: string,
+    wikipages: number [];
+}
+
+//tipagem do schema user 
 export interface InterfaceConfig{
     backgroundImage: string,    
-    favorite: string,
+    favorite: [ string ],
     fontFamily: string,
+    folders: InterfaceFolders[],
     outputMethod: 'sameScreen' | 'diffScreen', // sameScreen na mesma tela do inicio
                                                // diffScreen redireciona para tela de resultados  
+    quickAccess: [ string ],
     themeColor: string
 }
+
+const folderSchema = new Schema <InterfaceFolders>(
+    {
+        folderName: {
+            type: String,
+            required: false
+        },
+        wikipages: {
+            type: [ Number ],
+            default: []
+        }
+    },
+    { _id : false }
+)
 
 export const configSchema = new Schema<InterfaceConfig>( // garantindo que o schema seja equivalente a interface
     {
@@ -18,24 +41,32 @@ export const configSchema = new Schema<InterfaceConfig>( // garantindo que o sch
             type: String,
             default: ''
         },
-        favorite: {
-            type: String,
-            default: ''
+        favorite: { // lista
+            type: [ String ],
+            default: () => []
         },
         fontFamily: {
             type: String,
             default: ''
         },
+        folders: { // vai ser uma lista de objetos. dentro de folders -> folder -> wikipages (id : inteiro)
+            type: [ folderSchema ],
+            default: []
+        },
         outputMethod: {
             type: String,
-            enum: ['sameScreen', 'diffScreen'], 
+            enum: ['sameScreen', 'diffScreen'],
             default: 'diffScreen'
+        },
+        quickAccess: {
+            type: [ String ],
+            default: () => []
         },
         themeColor: {
             type: String,
             default: ''
         }
     },
-    { _id: false } // nao precisamos de um id para config ja vai estar importado em user
+    { _id: false }, // nao precisamos de um id para config ja vai estar importado em user
 )
 
