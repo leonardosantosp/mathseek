de09490e566e33150db6ac2ccffe6080a63e7a64
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-28 12:39:40 -0300

    feat: finishing login route

--- back-end/src/routes/login.ts
+++ back-end/src/routes/login.ts
@@ -2,63 +2,29 @@ import { verifyPassword } from '../utils/hash'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
+import { UserSchema } from '../models/user.schema'
 
 
 export function loginRoute(app) {
   dotenv.config()
 
-
-  // loga usuario e gera access_token e refreseh_token 
+  // loga usuario e gera access_token
   app.post('/login', async (req, res) => {
-    const { username, password } = req.body
-    const user = { username: username }; // melhor so incluir username na autenticacao
+    const { username, password } = req.body // na geracao do token apenas o username
 
-    
-    // TO-DO: autenticacao da senha
+    const isUser = await UserSchema.findOne({ username : username }); // password = password
 
+    if ( !isUser ) return res.status(404).send ({ message : 'Usuario nao encontrado' })
 
-    // na teoria essa senha encriptada ja pode ser gerada quando o usuario se cadastrar
-    //TO-DO: fazer ecrpt no sign in
-    async function hashPassword (password: string ) {
-      const salt = await bcrypt.genSalt(10) // valor de salt = 10
-      return bcrypt.hash(password, salt)
-    }
+    const isPassword =  await verifyPassword(password, isUser.hashedPassword)
 
-    const hashedPassword = await hashPassword(password);
+    if ( !isPassword ) return res.stauts(400).send({ message: 'Senha invalida' })
 
-    //
+    const access_token = jwt.sign({username: username}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3m'});
 
-    // verificando senha passada no body com senha encrypt
-    async function verifyPassword ( password: string, hash: string ) {
-      return bcrypt.compare ( password, hash)
-    }
-
-    const isPassword =  await verifyPassword(password, hashedPassword)
-
-    if ( !isPassword ) return res.stauts(400).send({ message: 'Invalid Password' })
-    
-    // const hashPassword =
-    //   '$2b$10$/OjRjRxm2rGDV41dzgHZzO8V1T4KuzxNh6tlcdBmQr6MJhWWA6ZCG'
-
-    // const isPassword = await verifyPassword(password, hashPassword)
-
-    // if (!isPassword) {
-    //   return res.status(400).send({ message: 'Invalid Password' })
-    // }
-
-    const access_token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn : '3m'});
-
-    const refresh_token = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn : '10m' });
-
-    return res.status(200).send({ user: user,password: password, hashedPassword: hashedPassword, isPassword: isPassword, access_token: access_token, refresh_token: refresh_token, accessSecret: process.env.ACCESS_TOKEN_SECRET, refreshSecret:process.env.REFRESH_TOKEN_SECRET })
+    res.send({user : isUser, access_token: access_token});
   })
 
-
-  // TO-DO: melhorar implementacao
-  // gera o access_token 
-  // function generateAccessToken ( user ) {
-  //   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn : '3m' })
-  // }
 
   app.post('/refresh', async (req, res) => {
     try {
