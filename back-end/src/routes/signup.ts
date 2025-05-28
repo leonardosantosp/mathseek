2e83ec6631ac133d3e787146291f087446bcb49a
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-28 12:40:07 -0300

    feat: finishing signin route

--- back-end/src/routes/signup.ts
+++ back-end/src/routes/signup.ts
@@ -1,40 +1,37 @@
 import { UserSchema } from '../models/user.schema'
 import { hashPassword } from '../utils/hash'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
 // calls para criacao de usuario
 export function signRoute(app) {
+  dotenv.config()
+
   // sign um usuario
-  app.post('/signup', async (req, res) => {
+  app.post('/signup', async (req, reply) => {
     try{
       const { username, email, password } = req.body
 
+       // verificar ser usuario eh unico
+      const userExist = await UserSchema.findOne({ username : username })
+
+      if ( userExist != undefined ) return reply.code(409).send({ message : 'Usuario ja existe'})
+
       const hashedPassword = await hashPassword(password)
 
-      // criando usuario maneira 1 
       const user = await UserSchema.create({
         username: username,
         email: email,
-        password: password,
-        hashedPassword: hashedPassword
-        // avatar
-        // status
-        // config
+        hashedPassword: hashedPassword,
       })
 
-      // criando usuario maneira 2 
-      // const user = new UserSchema({
-      //   username: username,
-      //   email: email,
-      //   password: password,
-      //   hashedPassword: hashedPassword
-      // })
+      // sempre jogar um objeto em jwt.sign. se jwt.sign(username, process...) -> erro 500 internal server error
+      const access_token = jwt.sign({ username: username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3m'});
 
-      // const isSaved = await user.save();
-
-      return res.status(201).send(user);
+      return reply.code(201).send({ user : user, access_token: access_token });
 
     } catch ( err ) {
-      return res.status(500).send({ message : err });
+      return reply.code(500).send({ message : err });
     }
   })
 }
