afd0d30ff0e185722f148bb81aec7407a1b65aad
leonardosantosp <leo040604@gmail.com>
2025-06-03 09:15:09 -0300

    refactor(chat): split chat-bubble into smaller components and improve loading bubble styles for dark/light modes

--- front-end/src/components/PanelChatBot.tsx
+++ front-end/src/components/PanelChatBot.tsx
@@ -1,148 +1,28 @@
-import {
-  SendHorizonal,
-  MessageCircleMore,
-  UserCircle2,
-  LinkIcon
-} from 'lucide-react'
+import { SendHorizonal, MessageCircleMore } from 'lucide-react'
 
-import chatIcon from '../assets/chat-bot-icon.png'
-import { SearchDocument } from './SearchDocument'
+import { ChatBubble } from './ChatBubble'
 
 export const PanelChatBot = () => {
   return (
     <div className="panel__search-mode">
       <div className="search-mode__chat">
         <div className="search-mode__chat--response">
-          <div className="chat--response-item">
-            <img src={chatIcon} alt="" />
-
-            <div className="chat-bubble">
-              Aqui estão alguns resultados:
-              <div className="bot-results">
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="chat--response-item">
-            <img src={chatIcon} alt="" />
-
-            <div className="chat-bubble">
-              Aqui estão alguns resultados:
-              <div className="bot-results">
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="chat--response-item">
-            <img src={chatIcon} alt="" />
-
-            <div className="chat-bubble">
-              Aqui estão alguns resultados:
-              <div className="bot-results">
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-              </div>
-            </div>
-          </div>
-
-          {/*  */}
-          <div className="chat--response-item">
-            <img src={chatIcon} alt="" />
-
-            <div className="chat-bubble">
-              Aqui estão alguns resultados:
-              <div className="bot-results">
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-                <span>
-                  <LinkIcon size={10} />
-                  Como Posso melhorar o desempenho do meu Notebook?
-                </span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <SearchDocument />
-          </div>
+          <ChatBubble type="response" />
+          <ChatBubble type="response" />
+          <ChatBubble type="response" />
+          <ChatBubble type="response" />
+          <ChatBubble type="load" />
         </div>
         <div className="search-mode__chat--query">
-          <div className="chat--query-item">
-            <UserCircle2 />
-            <div className="chat-bubble">
-              Como faço para evitar que meu notebook fique lento depois de
-              algumas horas ligado?
-            </div>
-          </div>
-
-          {/*  */}
-          <div className="chat--query-item">
-            <UserCircle2 />
-            <div className="chat-bubble">
-              Como faço para evitar que meu notebook fique lento depois de
-              algumas horas ligado?
-            </div>
-          </div>
-          <div className="chat--query-item">
-            <UserCircle2 />
-            <div className="chat-bubble">
-              Como faço para evitar que meu notebook fique lento depois de
-              algumas horas ligado?
-            </div>
-          </div>
-          <div className="chat--query-item">
-            <UserCircle2 />
-            <div className="chat-bubble">
-              Como faço para evitar que meu notebook fique lento depois de
-              algumas horas ligado?
-            </div>
-          </div>
-          <div className="chat--query-item">
-            <UserCircle2 />
-            <div className="chat-bubble">
-              Como faço para evitar que meu notebook fique lento depois de
-              algumas horas ligado?
-            </div>
-          </div>
+          <ChatBubble type="query" />
+          <ChatBubble type="query" />
+          <ChatBubble type="query" />
+          <ChatBubble type="query" />
+          <ChatBubble type="query" />
+          <ChatBubble type="query" />
         </div>
       </div>
+
       <div className="input-wrapper">
         <div className="input-container">
           <input type="text" placeholder="Ask me a question" />
