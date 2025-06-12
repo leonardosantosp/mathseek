80ca9e381702e1ab592a9878f71a10d3ff55b5d2
Leonardo Paiva <leo040604@gmail.com>
2025-06-12 20:49:28 -0300

    feat(user): add update history function to user service

--- back-end/src/controllers/history.controller.ts
+++ back-end/src/controllers/history.controller.ts
@@ -1,16 +1,35 @@
-import { getHistoryService } from '../services/history.service'
+import {
+  getHistoryService,
+  updateHistoryService
+} from "../services/history.service";
 
 export const getHistoryController = async (request, reply) => {
-  const id = request.user?.id
+  const id = request.user?.id;
 
   try {
-    const history = await getHistoryService(id)
-    return reply.code(200).send(history)
+    const history = await getHistoryService(id);
+    return reply.code(200).send(history);
   } catch (error) {
-    if (error instanceof Error && error.message === 'User not found') {
-      return reply.code(404).send({ message: 'user not found' })
+    if (error instanceof Error && error.message === "User not found") {
+      return reply.code(404).send({ message: "user not found" });
     }
-    console.error(error)
-    return reply.code(500).send({ message: 'Internal server error' })
+    console.error(error);
+    return reply.code(500).send({ message: "Internal server error" });
   }
-}
+};
+
+export const updateHistoryController = async (request, reply) => {
+  const id = request.user?.id;
+  const newHistory = request.body;
+
+  try {
+    const updatedUser = await updateHistoryService(id, newHistory);
+    return reply.code(200).send(updatedUser);
+  } catch (error) {
+    if (error instanceof Error && error.message === "User not found") {
+      return reply.code(404).send({ message: "User not found" });
+    }
+    console.error(error);
+    return reply.code(500).send({ message: "Internal server error" });
+  }
+};
--- back-end/src/services/history.service.ts
+++ back-end/src/services/history.service.ts
@@ -1,7 +1,18 @@
-import { getUserById } from '../repository/user.repository'
+import { getUserById, updateUser } from "../repository/user.repository";
+import { UpdateHistoryDto } from "../dto/history/updateHistory.dto";
 
 export const getHistoryService = async (id: string) => {
-  const user = await getUserById(id)
-  if (!user) throw new Error('User not found')
-  return user.history
-}
+  const user = await getUserById(id);
+  if (!user) throw new Error("User not found");
+  return user.history;
+};
+
+export const updateHistoryService = async (
+  id: string,
+  updateHistory: UpdateHistoryDto
+) => {
+  const user = await getUserById(id);
+  if (!user) throw new Error("User not found");
+  user.history = updateHistory;
+  return await updateUser(id, user);
+};
