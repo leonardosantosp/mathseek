41b4e71b93e95aad69f680d1f276612b0e6641fd
Leonardo Paiva <leonardo.santos@neuronex.tec.br>
2025-06-24 21:03:18 -0300

    feat: integrate login with error handling and store tokens in sessionStorage and cookies

--- back-end/src/dto/login/createLogin.dto.ts
+++ back-end/src/dto/login/createLogin.dto.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const createLoginDto = z.object({
-  username: z.string().email(),
+  username: z.string(),
   password: z.string()
 });
--- front-end/src/api-client/auth.ts
+++ front-end/src/api-client/auth.ts
@@ -5,7 +5,32 @@ type Login = {
   password: string;
 };
 
-export const login = async (user: Login) => {
+type User = {
+  _id: string;
+  username: string;
+  email: string;
+  status: string;
+  history: number[];
+  avatar: string;
+  hashedPassword: string;
+  config: {
+    backgroundImage: string;
+    fontFamily: string;
+    outputMethod: string;
+    themeColor: string;
+    favorite: number[];
+    folders: {
+      folderName: string;
+      wikipages: number[];
+    };
+    quickAccess: number[];
+  };
+  isPassword: boolean;
+  accessToken: string;
+  refreshToken: string;
+};
+
+export const login = async (user: Login): Promise<User> => {
   const response = await API.post("/login", user);
   return response.data;
 };
