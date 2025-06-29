6813b3b8fa76469ec51160908c89d28af826c412
Leonardo Paiva <leo040604@gmail.com>
2025-06-29 16:54:48 -0300

    fix: correct logo in page signup/login

--- front-end/src/pages/SignUpPage.tsx
+++ front-end/src/pages/SignUpPage.tsx
@@ -1,39 +1,40 @@
-import { Link } from 'react-router-dom'
-import { Login } from '../components/Login'
-import { useState } from 'react'
+import { Link } from "react-router-dom";
+import { Login } from "../components/Login";
+import { useState } from "react";
+import piLogo from "../public/assets/piLogo.png";
 
 export const LoginPage = () => {
-  const [loginType, setLoginType] = useState<'login' | 'signup' | 'forgot'>(
-    'login'
-  )
+  const [loginType, setLoginType] = useState<"login" | "signup" | "forgot">(
+    "login"
+  );
 
   return (
     <>
       <div className="login__header">
         <Link to="/">
           <div className="header__logo">
-            <span>π</span>
+            <img src={piLogo} alt="pi logo" />
             <h1>MathSeek</h1>
           </div>
         </Link>
       </div>
-      {loginType === 'login' ? (
+      {loginType === "login" ? (
         <Login
           title="Welcome Back !"
           subtitle="Skip the lag?"
           titleCard="Login"
           subtitleCard="Glad you’re back!"
           type="login"
-          setType={(type: 'login' | 'signup' | 'forgot') => setLoginType(type)}
+          setType={(type: "login" | "signup" | "forgot") => setLoginType(type)}
         />
-      ) : loginType === 'signup' ? (
+      ) : loginType === "signup" ? (
         <Login
           title="Roll the Carpet!"
           subtitle="Skip the lag ?"
           titleCard="Signup"
           subtitleCard="Just some details to get you in.!"
           type="signup"
-          setType={(type: 'login' | 'signup' | 'forgot') => setLoginType(type)}
+          setType={(type: "login" | "signup" | "forgot") => setLoginType(type)}
         />
       ) : (
         <Login
@@ -42,9 +43,9 @@ export const LoginPage = () => {
           titleCard="Forgot Password ?"
           subtitleCard="Please enter you’re email"
           type="forgot"
-          setType={(type: 'login' | 'signup' | 'forgot') => setLoginType(type)}
+          setType={(type: "login" | "signup" | "forgot") => setLoginType(type)}
         />
       )}
     </>
-  )
-}
+  );
+};
